684c22f1d32aa893bb14e4b1582318be7bf22fbd
Anjal <anjalraisam04@gmail.com>
2023-02-23 00:16:32 +0530

    added_mathjs

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 function parser(params) {
-    
+
+}
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.parent = null;
+        this.child = null;
+    }
+}
+
+class Tree{
+    constructor(){
+        this.head = null;
+        this.roots = null;
+        this.height = 0;
+    }
+    find(val){
+
+    }
+
+    insert(val){
+
+    }
+
+    remove(val){
+
+    }
+
+    remove(index){
+
+    }
 }
